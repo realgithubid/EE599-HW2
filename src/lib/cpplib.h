dd3b9adcd257ba0ed419c4cd73397a2daa688471
tieming <tiemings@usc.edu>
2020-09-05 22:17:56 -0700

    fix typo

--- src/lib/cpplib.h
+++ src/lib/cpplib.h
@@ -22,15 +22,15 @@ class CPPLib {
   std::vector<int> Flatten2DVector(const std::vector<std::vector<int>>&input);
 
   //q5
-  void CPPLib::Q5ReverseString(std::string &input);
-  std::vector<int> CPPLib::Q5ReverseVector(std::vector<int> input);
-  void CPPLib::Q5ToLower(std::string& input);
+  void Q5ReverseString(std::string &input);
+  std::vector<int> Q5ReverseVector(std::vector<int> input);
+  void Q5ToLower(std::string& input);
 
   //q6 
   bool canBePalindrome(const std::string &str);
 
   //q7
-  std::map<char, char> CPPLib::CalFactorial(const std::string& from, const std::string& to);
+  std::map<char, char> CalFactorial(const std::string& from, const std::string& to);
 
   //q8
   void kthPeek(std::vector<int> &input, int k);
